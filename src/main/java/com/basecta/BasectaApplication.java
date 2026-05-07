43ee62c932349fc8cdbf1b28af3513e46af4f9e8
Norbert Krupa <c21518659@mytudublin.ie>
2026-05-07 15:22:48 +0100

    initialized Spring Boot project

---
+++ src/main/java/com/basecta/BasectaApplication.java
@@ -0,0 +1,13 @@
+package com.basecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasectaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasectaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basecta/BasectaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basecta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasectaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
